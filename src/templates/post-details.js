d27c13ad0ce2e74c9126c3cd55a29cef07cfbe3c
Anirban Kar <thecodacus@gmail.com>
2021-12-14 02:13:32 +0530

    post details styling added

--- src/templates/post-details.js
+++ src/templates/post-details.js
@@ -1,21 +1,25 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import * as styles from "../styles/post-details.module.css"
+import * as styles from "../styles/post-details.module.scss"
 import { graphql } from "gatsby"
 
 export default function PostDetails({ data }) {
 	console.log(data)
 	const { html } = data.markdownRemark
-	const { title, category, featuredImage } = data.markdownRemark.frontmatter
+	const { title, category, featuredImage, date } = data.markdownRemark.frontmatter
 
 	return (
 		<Layout>
 			<div className={styles.details}>
-				<h2>{title}</h2>
-				<h3>{category}</h3>
+				<h1>{title}</h1>
 				<div className={styles.featured}>
 					<GatsbyImage image={getImage(featuredImage)} alt={title} />
+				</div>
+				<div className={styles.metadata}>
+					<h3>{new Date(date).toDateString()}</h3>
+					<h3>.</h3>
+					<h3>{category}</h3>
 				</div>
 				<div className={styles.html} dangerouslySetInnerHTML={{ __html: html }}></div>
 			</div>
