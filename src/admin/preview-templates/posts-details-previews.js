2338a5a8931345b31800993140183110c2f9baab
Anirban Kar <thecodacus@gmail.com>
2022-01-07 20:42:00 +0530

    updated

--- src/admin/preview-templates/posts-details-previews.js
+++ src/admin/preview-templates/posts-details-previews.js
@@ -2,23 +2,23 @@ import React from "react"
 import PropTypes from "prop-types"
 import Article from "../../components/Article"
 
-import Prism from "prismjs"
-// customize markdown-it
-const options = {
-	html: true,
-	typographer: true,
-	linkify: true,
-	highlight: function (str, lang) {
-		// var languageString = "language-" + lang
-		if (Prism.languages[lang]) {
-			return `<pre class="language-${lang} line-numbers"><code class="language-${lang}">${Prism.highlight(str, Prism.languages[lang], lang)}</code></pre>`
-		} else {
-			return `<pre class="language-${lang} line-numbers"><code class="language-${lang}">${Prism.util.encode(str)}</code></pre>`
-		}
-	},
-}
+// import Prism from "prismjs"
+// // customize markdown-it
+// const options = {
+// 	html: true,
+// 	typographer: true,
+// 	linkify: true,
+// 	highlight: function (str, lang) {
+// 		// var languageString = "language-" + lang
+// 		if (Prism.languages[lang]) {
+// 			return `<pre class="language-${lang} line-numbers"><code class="language-${lang}">${Prism.highlight(str, Prism.languages[lang], lang)}</code></pre>`
+// 		} else {
+// 			return `<pre class="language-${lang} line-numbers"><code class="language-${lang}">${Prism.util.encode(str)}</code></pre>`
+// 		}
+// 	},
+// }
 
-const customMarkdownIt = new markdownIt(options)
+// const customMarkdownIt = new markdownIt(options)
 
 const PostDetailsPreview = ({ entry, widgetFor, getAsset }) => {
 	//  id, html, excerpt, title, category, featuredImage, date, publicURL
@@ -29,10 +29,10 @@ const PostDetailsPreview = ({ entry, widgetFor, getAsset }) => {
 	console.log("featuredImage:", featuredImage)
 	const publicURL = featuredImage ? getAsset(featuredImage)?.toString() : null
 	const bodyWidget = widgetFor("body")
-	let bodyRendered = ""
-	if (bodyWidget) {
-		bodyRendered = customMarkdownIt.render(bodyWidget.props?.value || "")
-	}
+	// let bodyRendered = ""
+	// if (bodyWidget) {
+	// 	bodyRendered = customMarkdownIt.render(bodyWidget.props?.value || "")
+	// }
 	return (
 		<Article
 			{...{
@@ -43,7 +43,7 @@ const PostDetailsPreview = ({ entry, widgetFor, getAsset }) => {
 				featuredImage,
 				date,
 				publicURL,
-				html: bodyRendered,
+				bodyWidget,
 			}}
 		></Article>
 	)
--- src/components/Article.js
+++ src/components/Article.js
@@ -22,7 +22,7 @@ export default function Article({ html, bodyWidget, title, category, featuredIma
 					<img className={styles.alternateImage} src={featuredImagePath} alt={title} />
 				)}
 			</div>
-			<div className={styles.html} dangerouslySetInnerHTML={{ __html: html }}></div>
+			{html ? <div className={styles.html} dangerouslySetInnerHTML={{ __html: html }}></div> : { bodyWidget }}
 
 			{children}
 		</article>
